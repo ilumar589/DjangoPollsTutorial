7c3858fad5a0204b8b8a7abdee979f10b085d139
eduard.parvu <eduard.parvu@gentlab.com>
2016-02-15 17:42:13 +0200

    some changes

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -7,6 +10,13 @@ class Question(models.Model):
 
     def __str__(self):              # __unicode__ on Python 2
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
